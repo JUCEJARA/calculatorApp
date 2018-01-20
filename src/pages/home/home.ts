0a9964d33c5a57f6e5f30cbb8667679eb9f6fa20
anilkunchalaece@gmail.com <anilkunchalaece@gmail.com>
2018-01-20 21:57:15 +0530

    completed app

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,32 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  numberA;
+  numberB;
+  typeOfAction;
+  result;
   constructor(public navCtrl: NavController) {
+
+  }
+
+  performFunction(type){
+    var a = parseInt(this.numberA);
+    var b = parseInt(this.numberB);
+    console.log(type);
+
+    if( type == 'a'){
+      this.typeOfAction = "Add"
+      this.result = a+b
+    }else if(type == 's'){
+      this.typeOfAction = "SUB"
+      this.result = a - b;
+    }else if(type == 'm'){
+      this.typeOfAction = "MUL"
+      this.result = a *b
+    }else if(type == 'd'){
+      this.typeOfAction = "DIV"
+      this.result = a/b
+    }
 
   }
 
